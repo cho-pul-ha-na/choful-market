351e303a28e082f488454a5224e84bf497a4de3c
alstjr5949 <alstjr5949@gmail.com>
2022-06-27 17:25:19 +0900

    Feat: 초기 세팅 완료 1. 리액트 앱 초기 세팅 2. storybook, styled-components, styled-reset, react-router-dom 설치 3. .prettierrc 설정 4. GlobalStyle 설정 5. 커밋 템플릿 설정

---
+++ src/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import reset from 'styled-reset';
+import App from './App';
+import { mainTheme } from './theme';
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  html {
+    font-size: 10px;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  button {
+    width: 100%;
+    background: none;
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+  }
+  .ir {
+  display: block;
+  overflow: hidden;
+  position: absolute;
+  text-indent: -9999px;
+  line-height: 0;
+  font-size: 1px;
+  color: transparent;
+  }
+`;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={mainTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+);
---
+++ src/theme.js
@@ -0,0 +1,5 @@
+export const mainTheme = {
+  color: {
+    bgColor: '#FFF',
+  },
+};
