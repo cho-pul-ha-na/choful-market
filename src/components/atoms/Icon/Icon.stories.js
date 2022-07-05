6856fc1b04af69a77002ae9094134bbd741f2171
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-07-05 17:27:58 +0900

    FE-11 Chore : 아이콘 색상 변경 1. 프로젝트 시그니처 색상으로 스프라이트 이미지 내 아이콘 색상 변경 2. 스프라이트 파일에 맞게 스토리 값 수정

--- src/components/atoms/Icon/Icon.stories.js
+++ src/components/atoms/Icon/Icon.stories.js
@@ -7,10 +7,10 @@ export default {
 
 const Template = args => <Icon {...args} />;
 
-export const FabiconStory = Template.bind({});
-FabiconStory.args = {
+export const FaviconStory = Template.bind({});
+FaviconStory.args = {
     size: '16px',
-    xPoint: '-129px',
+    xPoint: '-126px',
     yPoint: '-236px',
     title: '파비콘',
 };
@@ -19,30 +19,38 @@ export const LargeUserIconStory = Template.bind({});
 LargeUserIconStory.args = {
     size: '48px',
     xPoint: '-10px',
-    yPoint: '-10px',
+    yPoint: '-78px',
     title: '큰 유저 아이콘',
 };
 
 export const UserIconStory = Template.bind({});
 UserIconStory.args = {
     size: '24px',
-    xPoint: '-146px',
-    yPoint: '-146px',
+    xPoint: '-142px',
+    yPoint: '-192px',
     title: '유저 아이콘',
 };
 
 export const FilledUserIconStory = Template.bind({});
 FilledUserIconStory.args = {
     size: '24px',
-    xPoint: '-192px',
-    yPoint: '-10px',
+    xPoint: '-186px',
+    yPoint: '-192px',
     title: '채워진 유저 아이콘',
 };
 
 export const LargeEditIconStory = Template.bind({});
 LargeEditIconStory.args = {
     size: '48px',
-    xPoint: '-78px',
+    xPoint: '-10px',
+    yPoint: '-10px',
+    title: '큰 게시글 추가 아이콘',
+};
+
+export const EditIconStory = Template.bind({});
+EditIconStory.args = {
+    size: '24px',
+    xPoint: '-192px',
     yPoint: '-10px',
     title: '큰 게시글 추가 아이콘',
 };
@@ -66,24 +74,24 @@ AlbumIconOffStory.args = {
 export const FacebookIconStory = Template.bind({});
 FacebookIconStory.args = {
     size: '24px',
-    xPoint: '-193px',
-    yPoint: '-54px',
+    xPoint: '-146px',
+    yPoint: '-102px',
     title: '페이스북 아이콘',
 };
 
 export const KakaoIconStory = Template.bind({});
 KakaoIconStory.args = {
     size: '24px',
-    xPoint: '-192px',
-    yPoint: '-142px',
+    xPoint: '-146px',
+    yPoint: '-146px',
     title: '카카오톡 아이콘',
 };
 
 export const GoogleIconStory = Template.bind({});
 GoogleIconStory.args = {
     size: '24px',
-    xPoint: '-192px',
-    yPoint: '-98px',
+    xPoint: '-102px',
+    yPoint: '-146px',
     title: '구글 아이콘',
 };
 
@@ -91,7 +99,7 @@ export const BackArrowIconStory = Template.bind({});
 BackArrowIconStory.args = {
     size: '22px',
     xPoint: '-236px',
-    yPoint: '-58px',
+    yPoint: '-55px',
     title: '뒤로가기 아이콘',
 };
 
@@ -105,41 +113,49 @@ LargeDeleteIcon.args = {
 
 export const DeleteIcon = Template.bind({});
 DeleteIcon.args = {
-    size: '20px',
+    size: '22px',
     xPoint: '-236px',
-    yPoint: '-11px',
+    yPoint: '-10px',
     title: '엑스모양 아이콘',
 };
 
 export const HeartIconStory = Template.bind({});
 HeartIconStory.args = {
     size: '20px',
-    xPoint: '-11px',
-    yPoint: '-236px',
+    xPoint: '-236px',
+    yPoint: '-179px',
     title: '하트모양 아이콘',
 };
 
 export const FilledHeartIconStory = Template.bind({});
 FilledHeartIconStory.args = {
-    size: '20px',
-    xPoint: '-51px',
+    size: '18px',
+    xPoint: '-50px',
     yPoint: '-236px',
     title: '채워진 하트모양 아이콘',
+};
+
+export const LargeHomeIconStory = Template.bind({});
+LargeHomeIconStory.args = {
+    size: '48px',
+    xPoint: '-78px',
+    yPoint: '-10px',
+    title: '홈 아이콘',
 };
 
 export const HomeIconStory = Template.bind({});
 HomeIconStory.args = {
     size: '24px',
-    xPoint: '-54px',
-    yPoint: '-192px',
+    xPoint: '-192px',
+    yPoint: '-54px',
     title: '홈 아이콘',
 };
 
 export const FilledHomeIconStory = Template.bind({});
 FilledHomeIconStory.args = {
     size: '24px',
-    xPoint: '-98px',
-    yPoint: '-192px',
+    xPoint: '-192px',
+    yPoint: '-98px',
     title: '채워진 홈 아이콘',
 };
 
@@ -154,31 +170,31 @@ ImageIconStory.args = {
 export const ImageLayerIconStory = Template.bind({});
 ImageLayerIconStory.args = {
     size: '20px',
-    xPoint: '-237px',
-    yPoint: '-180px',
+    xPoint: '-236px',
+    yPoint: '-139px',
     title: '이미지 레이어 아이콘',
 };
 
 export const MessageIconStory = Template.bind({});
 MessageIconStory.args = {
     size: '24px',
-    xPoint: '-186px',
-    yPoint: '-192px',
+    xPoint: '-192px',
+    yPoint: '-142px',
     title: '메시지 아이콘',
 };
 
 export const FilledMessageIconStory = Template.bind({});
 FilledMessageIconStory.args = {
     size: '24px',
-    xPoint: '-142px',
+    xPoint: '-10px',
     yPoint: '-192px',
     title: '채워진 메시지 아이콘',
 };
 
 export const NewNoticeIconStory = Template.bind({});
 NewNoticeIconStory.args = {
-    size: '6px',
-    xPoint: '-164px',
+    size: '12px',
+    xPoint: '-162px',
     yPoint: '-236px',
     title: '새로운 알림 아이콘',
 };
@@ -186,7 +202,7 @@ NewNoticeIconStory.args = {
 export const MoreIconOnStory = Template.bind({});
 MoreIconOnStory.args = {
     size: '18px',
-    xPoint: '-90px',
+    xPoint: '-88px',
     yPoint: '-236px',
     title: '활성화 된 더보기 아이콘',
 };
@@ -194,24 +210,24 @@ MoreIconOnStory.args = {
 export const MoreIconOffStory = Template.bind({});
 MoreIconOffStory.args = {
     size: '24px',
-    xPoint: '-146px',
-    yPoint: '-103px',
+    xPoint: '-54px',
+    yPoint: '-192px',
     title: '비활성화 된 더보기 아이콘',
 };
 
 export const ShareIconStory = Template.bind({});
 ShareIconStory.args = {
     size: '20px',
-    xPoint: '-236px',
-    yPoint: '-139px',
+    xPoint: '-10px',
+    yPoint: '-236px',
     title: '공유 아이콘',
 };
 
 export const SearchIconStory = Template.bind({});
 SearchIconStory.args = {
     size: '24px',
-    xPoint: '-102px',
-    yPoint: '-146px',
+    xPoint: '-98px',
+    yPoint: '-192px',
     title: '검색 아이콘',
 };
 
