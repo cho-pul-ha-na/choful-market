d8147fd79dbcbd4ae4f8b432f69da2fda9211e0c
alstjr5949 <alstjr5949@gmail.com>
2022-07-06 15:46:28 +0900

    FE-13 Feat : Button 컴포넌트 및 스토리 개발 1. Button 컴포넌트 및 스토리 개발 2. 재사용성을 위해 많은 props를 사용해서 구현해봤습니다.

---
+++ src/components/atoms/Button/Button.jsx
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  width: 100%;
+  font-size: ${props => props.fontSize};
+  font-weight: ${props => props.fontWeight};
+  line-height: ${props => props.lineHeight};
+  padding: ${props => props.padding};
+  background-color: ${props => props.bgColor};
+  color: ${props => props.txtColor};
+  border-radius: ${props => props.borderRadius};
+  &.btn_active {
+    background: ${props => props.theme.color.text.white};
+    outline: 1px solid ${props => props.theme.color.gray.d2};
+    color: ${props => props.theme.color.gray.d4};
+  }
+  &.btn_gallery {
+    width: 50px;
+    height: 50px;
+    background-image: url(${props => props.buttonImg});
+  }
+  &.btn_gallery-small {
+    width: 36px;
+    height: 36px;
+    background-image: url(${props => props.buttonImg});
+  }
+`;
+
+const Button = ({
+  label,
+  fontSize,
+  fontWeight,
+  lineHeight,
+  padding,
+  bgColor,
+  txtColor,
+  borderRadius,
+  buttonImg,
+  className,
+}) => {
+  return (
+    <StyledButton
+      type="button"
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      lineHeight={lineHeight}
+      padding={padding}
+      bgColor={bgColor}
+      txtColor={txtColor}
+      borderRadius={borderRadius}
+      buttonImg={buttonImg}
+      className={className}
+    >
+      {label}
+    </StyledButton>
+  );
+};
+
+export default Button;
