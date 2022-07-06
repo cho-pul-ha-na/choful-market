0ecf4c370e246c546c8ad7cdea9025692301bd2e
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-07-06 14:26:32 +0900

    Fix : git conflict 문제 해결 위한 커밋

--- src/components/atoms/SnsButton/SnsButton.jsx
+++ src/components/atoms/SnsButton/SnsButton.jsx
@@ -25,10 +25,7 @@ const SnsButton = styled.button`
 `
 
 const Sns = ({ SnsName, engName, src }) => {
-    return (
-    <SnsButton engName={engName} src={process.env.PUBLIC_URL + `/assets/${engName}.png`}>
-        {SnsName} 계정으로 로그인
-    </SnsButton>)
+    return <SnsButton engName={engName} src={process.env.PUBLIC_URL + `/assets/${engName}.png`}>{SnsName} 계정으로 로그인</SnsButton>
 }
 
 export default Sns;
