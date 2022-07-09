40380babf1d979277e74c82d489ee0427ad56b53
alstjr5949 <alstjr5949@gmail.com>
2022-07-09 22:07:40 +0900

    FE-18 Feat : Splash Screen 및 404 페이지 개발 1. Splash Screen 페이지를 개발했습니다. 2. 404 페이지를 개발했습니다.

---
+++ src/pages/SplashScreen/SplashScreen.jsx
@@ -0,0 +1,39 @@
+import styled, { keyframes } from 'styled-components';
+import Logo from '../../components/atoms/Logo/Logo';
+
+import FullLogoImg from '../../assets/full-logo.png';
+import { useState } from 'react';
+
+const fadeInOut = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(80px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  display: ${props => props.display};
+  justify-content: center;
+  margin-top: 246px;
+  animation: ${fadeInOut} 0.5s linear forwards;
+`;
+
+const SplashScreen = () => {
+  const [display, setDisplay] = useState('flex');
+
+  setTimeout(() => {
+    setDisplay('none');
+  }, 1500);
+  return (
+    <Wrapper display={display}>
+      <Logo size="200px" imgSrc={FullLogoImg} imgAlt="메인 로고 이미지"></Logo>
+    </Wrapper>
+  );
+};
+
+export default SplashScreen;
