55418ad6b48bf5dec40ef3b93e6768e580bcf49e
myeong-seok <ekauddl@naver.com>
2022-07-12 19:46:45 +0900

    FE-17 Feat : ChatList 페이지 제작 1. ChatList 페이지 제작

--- src/pages/ChatList.jsx
+++ src/pages/ChatList.jsx
@@ -1,5 +1,14 @@
+import UserInformation from '../components/modules/UserInformation/UserInformation'
+
+
 const ChatList = () => {
-  return null;
+  return (
+    <>
+      <UserInformation />
+      <UserInformation />
+      <UserInformation />
+    </>
+  );
 };
 
 export default ChatList;
