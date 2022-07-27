bb09411d9855eddcc1d002d1d5317e0239f9221a
myeong-seok <ekauddl@naver.com>
2022-07-27 21:51:12 +0900

    FE-40 Feat: 상품의 이미지 등록기능 개발 1. 상품의 이미지를 등록할 수 있도록 기능을 개발했습니다 2. Input 과 Label로  흩어져 있던 컴포넌트를 InputBox로 통합했습니다

--- src/pages/MyProfileAddProduct/MyProfileAddProduct.jsx
+++ src/pages/MyProfileAddProduct/MyProfileAddProduct.jsx
@@ -4,10 +4,18 @@ import Label from '../../components/atoms/Label/Label';
 import Input from '../../components/atoms/Input/Input';
 import Button from '../../components/atoms/Button/Button';
 import { CommonWrapper } from '../../components/common/commonWrapper';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import DefaultImg from '../../assets/product-default.png';
 import UploadImg from '../../assets/img-button.png';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import {
+  productImgAtom,
+  productLinkAtom,
+  productNameAtom,
+  productPriceAtom,
+} from '../../atoms';
+import axios from 'axios';
+import InputBox from '../../components/modules/InputBox/InputBox';
 
 const ProductForm = styled.form`
   display: flex;
@@ -33,29 +41,36 @@ const ProductForm = styled.form`
 `;
 
 const MyProfileAddProduct = () => {
-  const [productImgSrc, setProductImgSrc] = useState(DefaultImg);
+  const [productImgSrc, setProductImgSrc] = useRecoilState(productImgAtom);
+  const productName = useRecoilValue(productNameAtom);
+  const productPrice = useRecoilValue(productPriceAtom);
+  const productLink = useRecoilValue(productLinkAtom);
 
   const imgFileInput = useRef(null);
   // 버튼 클릭시 input 클릭 함수
-  const handleUploadImgBtnClick = () => {
+  const handleUploadImgBtnClick = e => {
+    e.preventDefault();
     imgFileInput.current.click();
   };
-  // input onChange시 프로필사진 바뀌게 하기
-  const handleImgInputOnchange = e => {
-    if (e.target.files[0]) {
-      setProductImgSrc(e.target.files[0]);
-    } else {
-      return;
+
+  const uploadImg = async imgFile => {
+    let formData = new FormData();
+    formData.append('image', imgFile);
+    try {
+      const res = await axios.post(
+        'https://mandarin.api.weniv.co.kr/image/uploadfile',
+        formData,
+      );
+      return `https://mandarin.api.weniv.co.kr/${res.data.filename}`;
+    } catch (error) {
+      console.log(error);
     }
-    // 이 부분은 아직 더 공부를 해야할 것 같습니다..ㅎ 내장함수라고 합니다!
-    const reader = new FileReader();
-    console.log(reader);
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        setProductImgSrc(reader.result);
-      }
-    };
-    reader.readAsDataURL(e.target.files[0]);
+  };
+
+  const handleProductImgInputOnchange = async e => {
+    const productImgSrc = await uploadImg(e.target.files[0]);
+    console.log(productImgSrc);
+    setProductImgSrc(productImgSrc);
   };
 
   return (
@@ -87,47 +102,33 @@ const MyProfileAddProduct = () => {
           type='file'
           className='ir'
           id='register-img'
-          accept='image/*'
-          onChange={handleImgInputOnchange}
+          accept='.jpg, .gif, .png, .jpeg, .bmp, .tif, .heic'
+          onChange={handleProductImgInputOnchange}
         />
-        <Label
-          fontWeight='500'
-          fontSize='12px'
-          lineHeight='15px'
+        <InputBox
           color={props => props.theme.color.gray.d4}
           label='상품명'
-          htmlFor='product-name'
-        />
-        <Input
           type='text'
           id='prodcut-name'
           placeholder='2~15자 이내여야 합니다.'
+          recoilKey={productNameAtom}
         />
-        <Label
-          fontWeight='500'
-          fontSize='12px'
-          lineHeight='15px'
+        <InputBox
           color={props => props.theme.color.gray.d4}
           label='가격'
           htmlFor='product-price'
-        />
-        <Input
           type='text'
           id='prodcut-price'
           placeholder='숫자만 입력 가능합니다.'
+          recoilKey={productPriceAtom}
         />
-        <Label
-          fontWeight='500'
-          fontSize='12px'
-          lineHeight='15px'
+        <InputBox
           color={props => props.theme.color.gray.d4}
           label='판매 링크'
-          htmlFor='product-url'
-        />
-        <Input
           type='text'
           id='prodcut-url'
           placeholder='URL을 입력해 주세요.'
+          recoilKey={productLinkAtom}
         />
       </ProductForm>
     </CommonWrapper>
