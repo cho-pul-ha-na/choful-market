c2825b668aa345f1e614c575579049422b592105
alstjr5949 <alstjr5949@gmail.com>
2022-07-28 22:40:57 +0900

    FE-47 Feat : recoil-persist 사용해 아톰의 상태유지 기능 개발
    1. recoil-persist를 사용해 새로고침이 되더라도 아톰의 값이 남아있게 하는 기능을 개발했습니다.

--- src/atoms.js
+++ src/atoms.js
@@ -1,7 +1,10 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
 import BasicProfileImg from './assets/basic-profile-img.png';
 import BasicProductImg from './assets/product-default.png';
+
+const { persistAtom } = recoilPersist();
 
 export const idValue = atom({
   key: 'userID',
@@ -11,11 +14,13 @@ export const idValue = atom({
 export const isEmailValidAtom = atom({
   key: 'isEmailValid',
   default: false,
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const passwordValue = atom({
   key: 'userPassword',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const postTxtValue = atom({
@@ -36,16 +41,19 @@ export const searchValue = atom({
 export const usernameValue = atom({
   key: 'username',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const accountnameValue = atom({
   key: 'accountname',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const userIntroValue = atom({
   key: 'userIntro',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const profileImgSrc = atom({
@@ -56,6 +64,7 @@ export const profileImgSrc = atom({
 export const isLogin = atom({
   key: 'isLogin',
   default: false,
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const userDataAtom = atom({
