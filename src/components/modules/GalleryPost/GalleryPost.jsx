a0bce5c14bfebe5d1238b19922bb05f4b69a95cd
daeun.Yang <75559282+dana-y@users.noreply.github.com>
2022-07-31 17:52:58 +0900

    FE-38 Fix : 프로필 페이지에서 갤러리 에러 수정
    1. 갤러리 모드에서 이미지가 여러장일 경우 에러가 나는 부분을 수정했습니다.
    2. 여러장일 경우 첫번째 이미지가 나타나고 이미지에 여러장임을 알려주는 갤러리 아이콘을 띄웁니다.

--- src/components/modules/GalleryPost/GalleryPost.jsx
+++ src/components/modules/GalleryPost/GalleryPost.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { Link, useLocation, useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import Icon from '../../atoms/Icon/Icon';
 import Img from '../../atoms/Img/Img';
 
 const GalleryWrapper = styled.div`
@@ -18,12 +19,14 @@ const GalleryWrapper = styled.div`
   }
 `;
 
+const ImgLink = styled(Link)`
+  position: relative;
+`;
+
 const GalleryPost = () => {
   const token = localStorage.getItem('token');
-
   const path = useLocation().pathname;
   const { id } = useParams();
-
   const [postData, setPostData] = useState([]);
 
   const getMyPostData = async () => {
@@ -47,19 +50,30 @@ const GalleryPost = () => {
     if (path.includes('yourProfile') || path.includes('profile')) {
       getMyPostData();
     }
+    console.log(postData);
   }, []);
 
   return (
     <GalleryWrapper>
       {postData.map(postData =>
         postData.image ? (
-          <Link to={`/post/${postData.id}`}>
+          <ImgLink to={`/post/${postData.id}`}>
             <Img
               key={postData.id}
-              imgSrc={postData.image}
+              imgSrc={postData.image.split(',')[0]}
               imgAlt='게시글 갤러리뷰 이미지'
             />
-          </Link>
+            <Icon
+              position='absolute'
+              top='5px'
+              right='5px'
+              size='20px'
+              xpoint='-236px'
+              ypoint='-139px'
+              title='여러장'
+              className={postData.image.split(',').length === 1 && 'hide'}
+            />
+          </ImgLink>
         ) : null,
       )}
     </GalleryWrapper>
