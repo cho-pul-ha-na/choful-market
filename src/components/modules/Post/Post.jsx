d6a1baf988a7102d01b92abd76fa77f4edbb0ccf
ThisisMiri <mirikim715@gmail.com>
2022-08-12 16:51:23 +0900

    FE-56 Refactor : import 정리 1. Post, YourProfileBtn.jsx 파일의 import를 정리하였습니다.

--- src/components/modules/Post/Post.jsx
+++ src/components/modules/Post/Post.jsx
@@ -1,6 +1,6 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Link, useLocation, useMatch, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 import Img from '../../atoms/Img/Img';
 import PostUserInfo from '../PostUserInfo/PostUserInfo';
